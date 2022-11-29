dfe8004bf4b5ed7bb2ce414de8ba4b0db8d4693a
Shakir Hanif <shakirhanif@hotmail.com>
2022-11-29 17:48:33 +0500

    products added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import { Layout } from "../components/Layout";
+import { ProductItems } from "../components/ProductItems";
+import data from "../utils/data";
 
 export default function Home() {
   return (
-    <div>
-      <Layout title="Home Page"></Layout>
-      <h1>hello</h1>
+    <div className=" grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      {data.products.map((pro) => {
+        return <ProductItems product={pro} key={pro.slug}></ProductItems>;
+      })}
     </div>
   );
 }
